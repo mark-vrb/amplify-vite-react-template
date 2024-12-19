b419d17b8b9b0ec5c0dcccd8a2937e509d0bcdf0
Mark Varabyou <m_var@icloud.com>
2024-12-19 12:32:24 -0800

    access enable_features env var in lambda

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,13 +1,15 @@
 import type { Handler } from 'aws-lambda';
+import { env } from '$amplify/env/say-hello'; // the import is '$amplify/env/<function-name>'
 
-export const handler: Handler = async (event, context) => {
-  const featureFlagsOnly: { [key: string]: string } = { "SOME_PREDEFINED_FLAG": 'true' };
+export const handler: Handler = async () => {
+  // const featureFlagsOnly: { [key: string]: string } = { "SOME_PREDEFINED_FLAG": 'true' };
 
-  for (const [key, value] of Object.entries(process.env)) {
-    if (key.startsWith('ENABLE_') && value !== undefined) {
-      featureFlagsOnly[key] = value.toString();
-    }
-  }
+  const features: string[] = env.ENABLE_FEATURES?.split(',');
+  // for (const [key, value] of Object.entries(process.env)) {
+  //   if (key.startsWith('ENABLE_') && value !== undefined) {
+  //     featureFlagsOnly[key] = value.toString();
+  //   }
+  // }
 
-  return JSON.stringify(featureFlagsOnly);
+  return features;
 };
