f2f0b772fe44a80a66634199ee9d753fb37ef7bc
Mark Varabyou <m_var@icloud.com>
2024-12-20 09:12:37 -0800

    demo fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,15 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [features, setFeatures] = useState<Array<Schema["EnabledFeatures"]["type"]>>([]);
-  const [, setFeaturesList] = useState<Array<string>>([]);
+  // const [features, setFeatures] = useState<Array<Schema["EnabledFeatures"]["type"]>>([]);
+
+  const [buildTimeFeatures, setBuildTimeFeatures] = useState<string>();
+  const [liveFeatures, setLiveFeatures] = useState<string>();
+
+  const [featuresList, setFeaturesList] = useState<string>();
+  const [featuresInitialized, setFeaturesInitialized] = useState<boolean>(false);
+
+  const dialog = document.querySelector("dialog");
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -17,22 +24,41 @@ function App() {
     });
 
     client.models.EnabledFeatures.observeQuery().subscribe({
-      next: (data) => setFeatures([...data.items]),
-    })
+      next: (data) => {
+        const features = JSON.parse(data.items[0]?.features as string || '[]');
+
+        if (Array.isArray(features)) {
+          setLiveFeatures(JSON.stringify(features));
+        }
+      }
+    });
 
     client.queries.getFeatureFlags().then(value => {
-      const buildTimeFeatureFlags: string[] = JSON.parse(value.data || '[]');
-      console.log('buildTime FFs:', buildTimeFeatureFlags)
+      const features = value.data || '[]';
+      setBuildTimeFeatures(features);
     })
   }, []);
 
   useEffect(() => {
-    const featuresDef = JSON.parse(features[0]?.features as string || '[]');
-    if (Array.isArray(featuresDef)) {
-      setFeaturesList([...featuresDef]);
-      console.log("features update received:", featuresDef);
+    if (!buildTimeFeatures || !liveFeatures) return;
+
+    const buildTime = JSON.parse(buildTimeFeatures) as string[];
+    const live = JSON.parse(liveFeatures) as string[];
+
+    const set = new Set([...buildTime, ...live]);
+    setFeaturesList(JSON.stringify(Array.from(set)));
+  }, [buildTimeFeatures, liveFeatures]);
+
+  useEffect(() => {
+    if (!featuresList) return;
+    if (featuresInitialized) {
+      console.log("features update received:", featuresList);
+      dialog?.showModal();
+    } else {
+      console.log("features initialized:", featuresList);
     }
-  }, [features]);
+    setFeaturesInitialized(true);
+  }, [dialog, featuresList, featuresInitialized]);
 
   function createTodo() {
     client.models.Todo.create({content: window.prompt("Todo content")});
@@ -40,6 +66,15 @@ function App() {
 
   function deleteTodo(id: string) {
     client.models.Todo.delete({id})
+  }
+
+  function refreshPage() {
+    dialog?.close();
+    document.location.reload();
+  }
+
+  function closeDialog() {
+    dialog?.close();
   }
 
   return (
@@ -59,6 +94,14 @@ function App() {
           </li>
         ))}
       </ul>
+
+      <dialog>
+        <p>New version of the app available!</p>
+        <div className="dialog-buttons">
+          <button className="secondary" onClick={closeDialog}>Cancel</button>
+          <button onClick={refreshPage}>Refresh</button>
+        </div>
+      </dialog>
     </main>
   );
 }
